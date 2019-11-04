91fb8579c54c348cc91d0e641c215d4d42e42983
Christianbenjaminsson <christianbenjaminsson@outlook.com>
2019-11-04 20:16:51 +0100

    Created 3 methods for: get input, print array and resize array

---
+++ ListAndArraylist.java
@@ -0,0 +1,43 @@
+package com.example.listandarraylist;
+
+import java.util.Scanner;
+
+public class ListAndArraylist {
+
+    private static Scanner scan = new Scanner(System.in);
+    private static int[] baseData = new int[10];
+
+    public static void main(String[] args) {
+        System.out.println("Enter 10 integers: ");
+        getInput();
+        printArray(baseData);
+        resizeArray();
+        System.out.println("Enter 12 integers: ");
+        getInput();
+        printArray(baseData);
+
+    }
+
+    private static void getInput()
+    {
+        for(int i = 0; i < baseData.length; i ++)
+            baseData[i] = scan.nextInt();
+    }
+
+    private static void printArray(int[] arr)
+    {
+        for(int i = 0; i < arr.length; i ++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    private static void resizeArray()
+    {
+        int[] original = baseData;
+
+        baseData = new int[12];
+        for(int i = 0; i < original.length; i ++)
+            baseData[i] = original[i];
+    }
+
+}
